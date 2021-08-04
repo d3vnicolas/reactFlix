b2687b3b1fc03c917c9ecdea84f3aa99d3d833a8
Nicolas Soares <devnicolas.coroa@gmail.com>
2021-08-04 12:29:52 -0300

    Primeira parte do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
 import './App.css';
+import FeatureMovie from './components/FeaturedMovie';
 
-function App() {
+const AppMain = () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //Pegando lista total
+
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      // Pegando o featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+      console.log(chosenInfo);
+    }
+
+    loadAll();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="page">
+
+      {featuredData &&
+        <FeatureMovie item={featuredData}/>
+      }
+
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default AppMain;
